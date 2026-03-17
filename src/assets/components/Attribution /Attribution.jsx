6a64017e6447eb04aeeefaaf34f30ce7e7142b87
Vitalij Lazarev <edovsky@hotmail.com>
2026-03-17 20:37:40 +0200

    Add Attribution component with styling

---
+++ src/assets/components/Attribution /Attribution.jsx
@@ -0,0 +1,24 @@
+import "./Attribution.scss";
+
+function Attribution() {
+  return (
+    <div className="attribution">
+      <p>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://www.frontendmentor.io/profile/p3lm3shka1"
+          target="_blank"
+        >
+          p3lm3shka1
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
+
+export default Attribution;
